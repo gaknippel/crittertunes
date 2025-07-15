83609d3255ac14fdfd5dd03426ee6749f7f61646
Greyson Knippel <gaknippel@hotmail.com>
2025-07-15 12:21:09 -0700

    added playing function, added rfd dependency to .toml

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,26 @@ use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
+use rfd::FileDialog;
 
 struct MyApp
 {
     is_playing: bool,
+    file_path: Option<PathBuf>,
+    #[allow(dead_code)]
+    stream: Option<OutputStream>,
+    sink: Option<Sink>,
 }
 
 
 impl Default for MyApp
 {
     fn default() -> Self {
-        Self {is_playing: false}
+        Self {is_playing: false,
+        file_path: None,
+        stream: None,
+        sink: None,
+    }
     }
 }
 
@@ -23,21 +32,50 @@ impl eframe::App for MyApp
     {
         egui::CentralPanel::default().show(ctx, |ui|
         {
-            let available = ui.available_size();
+            if ui.button("Load MP3").clicked()
+            {
+                if let Some(path) = FileDialog::new().add_filter("MP3", & ["mp3"]).pick_file()
+                {
+                    self.file_path = Some(path);
+                    self.is_playing = false;
+                    self.sink = None; //stop prev playback
+                }
+            }
+
+            if let Some(path) = &self.file_path
+            {
+                ui.label(format!("Loaded: {}", path.display()));
+                let available = ui.available_size();
             
             ui.add_space(available.y/ 2.0 - 20.0);
 
             ui.horizontal(|ui| 
 
-            {
+                    {
                 ui.add_space(available.x / 2.0 - 40.0);
-            if ui.button("Play").clicked()
-            {
-                self.is_playing = true;
-                //add stuff here to play music!
-            }
-            }
+
+                let mut play_button = egui::Button::new(egui::RichText::new("play!").size(32.0));
+
+            if ui.add(play_button).clicked() && !self.is_playing
+                {
+                if let Ok((stream, stream_handle)) = OutputStream::try_default()
+                {
+                    let sink = Sink::try_new(&stream_handle).unwrap();
+                    let file = BufReader::new(File::open(path).unwrap());
+                    let source = Decoder::new(file).unwrap();
+                    sink.append(source);
+                    sink.play();
+                    self.stream = Some(stream);
+                    self.sink = Some(sink);
+                    self.is_playing = true;
+                }
+                
+                }
+                    }
             );
+            }
+
+            
         });
     }
 }
